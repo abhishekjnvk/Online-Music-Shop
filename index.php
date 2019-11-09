c90b3cd7a9d1eff20c46e8080bdd8d90fb102e82
Abhishek Kumar <abhishekkumarjnvk@gmail.com>
2019-11-10 01:19:23 +0530

    Done with music player

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
-<?php    include('include/functions/function.php');
-    // include('include/functions/mail/index1.php');
-checklogin();
-// $response = sign_up("abhi","admin@admin.com","admin");
-
-echo "<script>alert(response)</script>";
-
+<?php
+include('include/functions/function.php');      //including file of functions
+checklogin();     //function to check weather user is logged in or not
 ?>
 <!doctype html>
 <html lang="en">
@@ -12,55 +8,67 @@ echo "<script>alert(response)</script>";
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap CSS -->
-    <meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
-<link rel='stylesheet' href='https://cdn.plyr.io/3.5.6/plyr.css'><link rel="stylesheet" href="./style.css">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel='stylesheet' href='https://cdn.plyr.io/3.5.6/plyr.css'>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Home</title>
 </head>
 
 <body style="background: url('include/bg2.jpg') repeat 0 0;">
     <?php  include('include/nav.php');?>
-        <div class="container bg-dark">
-
-          <div class="container bg-primary">
+    <!--including top navigation file -->
+    <div class="container bg-dark">
+        <div class="container bg-primary">
             <div class="column add-bottom">
-        <div id="mainwrap">
-            <div id="nowPlay">
-                <img src="" class="card-img-top mt-2" alt="Album Picture" id="album_picture"><br>
-                <span id="npAction">Paused...</span><span id="npTitle"></span><br>
-                <span id="npArtist">Abhishek Kumar</span>
-            </div>
-            <div id="audiowrap">
-                <div id="audio0">
-                    <audio id="audio1" preload controls>Your browser does not support HTML5 Audio! 😢</audio>
-                </div>
-                <div id="tracks">
-                    <a id="btnPrev">&vltri;</a><a id="btnNext">&vrtri;</a>
-                </div>
-            </div>
-            <div id="plwrap"><center>
-                <ul id="plList"></ul>
-                </center>
-            </div>
-        </div>
-    </div>
-    <div class="column add-bottom center">
-        <p>&copy Abhishek</a></p>
-    </div>
-</div>
 
+                <div id="mainwrap">
+                    <div id="nowPlay">
+                        <center><img src="" class="mt-2 mx-auto col-lg-4" alt="Album Picture" id="album_picture"><br> </center> <!--  Album picture goes here   -->
+                        <span id="npAction">Paused...</span><span id="npTitle"></span><br> <!--  Title of the song  -->
+                        <center><span id="npArtist"></span> </center><!--  Artist of the song goes here   -->
+                    </div>
+
+                    <div id="audiowrap" class="mt-3">
+                        <!-- Audio player -->
+                        <div id="audio0">
+                            <audio id="audio1" preload controls>Your browser does not support HTML5 Audio! 😢</audio> <!--  Text to displayed if browder doesnot support audio   -->
+                        </div>
+
+                        <!-- control button starts-->
+                        <div id="tracks" class="mt-3 col-lg-12">
+                            <a id="btnPrev"><button class="btn btn-warning border border-secondary mx-auto"><span style='font-size:40px;'>&#8592;</span></button></a>
+                            <a id="btnNext"><button class="btn btn-warning border border-secondary ml-2 mx-auto"><span style='font-size:40px;'>&#8594;</span></button></a>
+                        </div>
+                        <!-- control button Ends -->
+
+                    </div>
+
+                    <!-- Start of Song List-->
+                    <div id="plwrap" class="mt-5 border">
+                        <center>
+                            <ul id="plList" style="list-style-type:none;"></ul>
+                        </center>
+                    </div>
+                    <!-- End of song list -->
+
+                </div>
+            </div>
+            <div class="center mb-3">
+                <p>&copy Abhishek</a></p>
+            </div>
         </div>
+
+    </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script> -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'></script>
-  <script src='https://cdnjs.cloudflare.com/ajax/libs/html5media/1.1.8/html5media.min.js'></script>
-  <script src='https://cdnjs.cloudflare.com/ajax/libs/plyr/3.5.6/plyr.min.js'></script><script  src="./script.js"></script>
+    <!-- <script src='https://cdnjs.cloudflare.com/ajax/libs/html5media/1.1.8/html5media.min.js'></script> -->
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/plyr/3.5.6/plyr.min.js'></script>
+    <script src="./script.js"></script>
 
 </body>
 
--- script.js
+++ script.js
@@ -21,70 +21,70 @@ jQuery(function ($) {
         var index = 0,
             playing = false,
             tracks = [
-   {
-      "song":"Afreen Afreen",
-      "url":"http://hck.re/Rh8KTk",
-      "artists":"Rahat Fateh Ali Khan, Momina Mustehsan",
-      "cover_image":"http://hck.re/kWWxUI"
+                {
+                    "song": "Afreen Afreen",
+                    "url": "http://hck.re/Rh8KTk",
+                    "artists": "Rahat Fateh Ali Khan, Momina Mustehsan",
+                    "cover_image": "http://hck.re/kWWxUI"
    },
-   {
-      "song":"Aik Alif",
-      "url":"http://hck.re/ZeSJFd",
-      "artists":"Saieen Zahoor, Noori",
-      "cover_image":"http://hck.re/3Cm0IX"
+                {
+                    "song": "Aik Alif",
+                    "url": "http://hck.re/ZeSJFd",
+                    "artists": "Saieen Zahoor, Noori",
+                    "cover_image": "http://hck.re/3Cm0IX"
    },
-   {
-      "song":"Tajdar e haram",
-      "url":"http://hck.re/wxlUcX",
-      "artists":"Atif Aslam",
-      "cover_image":"http://hck.re/5dh4D5"
+                {
+                    "song": "Tajdar e haram",
+                    "url": "http://hck.re/wxlUcX",
+                    "artists": "Atif Aslam",
+                    "cover_image": "http://hck.re/5dh4D5"
    },
-   {
-      "song":"Aaj Rung",
-      "url":"http://hck.re/H5nMm3",
-      "artists":"Amjad Sabri,  Rahat Fateh Ali Khan",
-      "cover_image":"http://hck.re/U1bRnt"
+                {
+                    "song": "Aaj Rung",
+                    "url": "http://hck.re/H5nMm3",
+                    "artists": "Amjad Sabri,  Rahat Fateh Ali Khan",
+                    "cover_image": "http://hck.re/U1bRnt"
    },
-   {
-      "song":"Ae dil",
-      "url":"http://hck.re/2nCncK",
-      "artists":"Ali Zafar, Sara Haider",
-      "cover_image":"http://hck.re/eLtjUb"
+                {
+                    "song": "Ae dil",
+                    "url": "http://hck.re/2nCncK",
+                    "artists": "Ali Zafar, Sara Haider",
+                    "cover_image": "http://hck.re/eLtjUb"
    },
-   {
-      "song":"Man Amadeh am",
-      "url":"http://hck.re/epOzj9",
-      "artists":"Atif Aslam, Gul Panrra",
-      "cover_image":"http://hck.re/KvT2Vv"
+                {
+                    "song": "Man Amadeh am",
+                    "url": "http://hck.re/epOzj9",
+                    "artists": "Atif Aslam, Gul Panrra",
+                    "cover_image": "http://hck.re/KvT2Vv"
    },
-   {
-      "song":"Bewajah",
-      "url":"http://hck.re/YkbDDP",
-      "artists":"Nabeel Shaukat Ali",
-      "cover_image":"http://hck.re/N29EEt"
+                {
+                    "song": "Bewajah",
+                    "url": "http://hck.re/YkbDDP",
+                    "artists": "Nabeel Shaukat Ali",
+                    "cover_image": "http://hck.re/N29EEt"
    },
-   {
-      "song":"Dinae Dinae",
-      "url":"http://hck.re/dMquYY",
-      "artists":"Harshadeep Kaur",
-      "cover_image":"http://hck.re/6l9QqH"
+                {
+                    "song": "Dinae Dinae",
+                    "url": "http://hck.re/dMquYY",
+                    "artists": "Harshadeep Kaur",
+                    "cover_image": "http://hck.re/6l9QqH"
    },
-   {
-      "song":"Tera woh pyar",
-      "url":"http://hck.re/64Tzod",
-      "artists":"Momina Mustehsan, Asim Azhar",
-      "cover_image":"http://hck.re/rlYqJY"
+                {
+                    "song": "Tera woh pyar",
+                    "url": "http://hck.re/64Tzod",
+                    "artists": "Momina Mustehsan, Asim Azhar",
+                    "cover_image": "http://hck.re/rlYqJY"
    },
-   {
-      "song":"Shamaan Pai gaiyan",
-      "url":"http://hck.re/VhtQGh",
-      "artists":"Rachel Viccaji, Kashif Ali",
-      "cover_image":"http://hck.re/gs0grk"
+                {
+                    "song": "Shamaan Pai gaiyan",
+                    "url": "http://hck.re/VhtQGh",
+                    "artists": "Rachel Viccaji, Kashif Ali",
+                    "cover_image": "http://hck.re/gs0grk"
    }
 ],
 
 
-            buildPlaylist = $.each(tracks, function(key, value) {
+            buildPlaylist = $.each(tracks, function (key, value) {
                 var trackNumber = "",
                     trackName = value.song,
 
